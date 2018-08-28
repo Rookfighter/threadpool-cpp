13fb9b9c42d451ea1330b4dacc74fe2099cf454c
Fabian Meyer <user8324@posteo.net>
2018-08-28 15:04:36 +0200

    Add tests

---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 28 Aug 2018
+ *      Author: Fabian Meyer
+ */
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include "tpcpp/thread_pool.h"
---
+++ test/test_thread_pool.cpp
@@ -0,0 +1,9 @@
+/*
+ * test_thread_pool.cpp
+ *
+ *  Created on: 28 Aug 2018
+ *      Author: Fabian Meyer
+ */
+
+#include <catch.hpp>
+#include "tpcpp/thread_pool.h"
