aae83aefb5415b9b2a8ec74e4ac575a86f1acdba
Fabian Meyer <user8324@posteo.net>
2018-08-28 15:29:06 +0200

    Add a simple test

--- test/test_thread_pool.cpp
+++ test/test_thread_pool.cpp
@@ -7,3 +7,24 @@
 
 #include <catch.hpp>
 #include "tpcpp/thread_pool.h"
+
+using namespace tp;
+
+TEST_CASE("thread pool")
+{
+    ThreadPool pool(4);
+    REQUIRE(pool.size() == 4);
+
+    SECTION("execute tasks")
+    {
+        std::mutex mutex;
+        unsigned int actCnt = 0;
+        unsigned int expCnt = 4;
+
+        for(unsigned int i = 0; i < expCnt; ++i)
+            pool.enqueue([&actCnt, &mutex](){std::unique_lock<std::mutex>(mutex); ++actCnt;});
+        pool.wait();
+
+        REQUIRE(expCnt == actCnt);
+    }
+}
